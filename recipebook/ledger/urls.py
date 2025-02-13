2f2c255111687601795a97ac877d6d3725ed83a2
auby <joaquin.tirona@student.ateneo.edu>
2025-02-13 16:16:27 +0800

    Created templates for the views, implemented contexts

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import recipelist, recipe1, recipe2
 
 urlpatterns = [
-    path('/list', recipelist, name='recipe-list'),
-    path('/1', recipe1, name='recipe-1'),
-    path('/2', recipe2, name='recipe-2'),
+    path('list', recipelist, name='recipe-list'),
+    path('1', recipe1, name='recipe-1'),
+    path('2', recipe2, name='recipe-2'),
 ]
 
 app_name = 'ledger'
