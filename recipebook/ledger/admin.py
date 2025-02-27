1d2555e3f41a407a562485193a5b282b9ddadaa4
auby <joaquin.tirona@student.ateneo.edu>
2025-02-27 22:24:02 +0800

    code cleanup

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import Ingredient, Recipe, RecipeIngredient
 
+
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
+
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [RecipeIngredientInline]
 
+
 class IngredientAdmin(admin.ModelAdmin):
     model = Ingredient
+
 
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,35 +1,34 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ledger:recipe-detail', args=[self.pk])
+
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
     def get_absolute_url(self):
         return reverse('ledger:recipe-detail', args=[self.pk])
-    
-    class Meta:
-        verbose_name = 'recipe'
-        verbose_name_plural = 'recipes'
+
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredient = models.ForeignKey(Ingredient,
                                    related_name='recipe',
                                    on_delete=models.SET_NULL,
-                                   null = True,)
+                                   null=True,)
     recipe = models.ForeignKey(Recipe,
                                related_name='ingredients',
                                on_delete=models.SET_NULL,
-                               null = True,)
+                               null=True,)
