4ad2e1159690aae8c764ccd94a5752be7818386f
auby <joaquin.tirona@student.ateneo.edu>
2025-03-26 12:16:09 +0800

    Added RecipeImage TabularInline to RecipeAdmin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
-from .models import Ingredient, Recipe, RecipeIngredient
+from .models import Ingredient, Recipe, RecipeIngredient, RecipeImage
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
+
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeIngredientInline]
+    inlines = [RecipeIngredientInline, RecipeImageInline]
 
 
 class IngredientAdmin(admin.ModelAdmin):
